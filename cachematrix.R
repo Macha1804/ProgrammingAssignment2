e609ba22529de6820a972e590f8cecdd9d284765
Niangoran Renee Michele <rniangoran@gmail.com>
2024-12-30 10:58:29 +0100

    Exercice fait

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,16 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+if(!is.matrix(mat)) {stop("L'entrée doit être une matrice.")}
+f <- NULL
+set <- function(y) {
+  mat <<- y
+  f <<- NULL
+}
+get <- function() x {
+setcache <- function(solve) f <<- inverse
+getcache <- function () f
+list(set = set,get = get, setcache = setcache, getcache = getcache)
 }
 
 
@@ -12,4 +21,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        f <- x$getcache()
+        if(!is.null(f)){
+          message('getting cached data')
+          return(f)
+        }
+        data <- x$get()
+        f <- solve(data)
+        x$setcache(f)
+        f
 }
